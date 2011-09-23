78a9faee9e1c74ed870507f4a70f5fa76223f212
Viko Nava <viko.nava@gmail.com>
2011-09-23 15:08:53 -0700

    Initial con Sorcery Authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudSchoolComputing::Application.config.secret_token = 'eedc73c489a9e63d3ceef13dc616b67ba97761422ea9c8155a1342be07c2a756a7169b9308ed4052bf96f0f6293caf38b1ebadfd90c91d548286240cf3e035a9'
