ede499c81440a61a6d4f13e4a7f5972277c63b9b
Viko Nava <viko.nava@gmail.com>
2011-11-23 11:31:14 -0600

    Fix abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -58,6 +58,57 @@ class Ability
       can :manage, Activity do |a|
 	a.group.school.owner == user
       end
+    elsif user.assistant?
+      package = user.school.owner.package
+
+      users = 0
+      School.find(:all, :conditions => { :owner_id => user.id }).each do |school|
+        users = users + school.users.count
+      end
+      can [:create,:new], User if package.users_limit > users
+      can [:index,:read,:show, :more], User do |u|
+	u == user || u.school.owner == user
+      end
+
+      can [:create,:new], School
+      can :manage, School do |s|
+	s == user.school
+      end
+
+      can [:index,:read,:show,:edit,:update,:destroy], Student do |s|
+    	s.school.owner == user
+      end
+
+      subjects = 0
+      School.find(:all, :conditions => { :owner_id => user.id }).each do |school|
+        subjects = subjects + school.subjects.count
+      end
+      can [:create,:new], Subject if package.subjects_limit > subjects
+      can [:index,:read,:show,:edit,:update,:destroy], Subject do |s| 
+	s.school == user.school
+      end
+
+      groups = 0
+      School.find(:all, :conditions => { :owner_id => user.id }).each do |school|
+        groups = groups + school.groups.count
+      end
+      can [:create,:new], Group if package.groups_limit > groups
+      can [:index,:read,:show,:edit,:update,:destroy], Group do |g|
+	g.school == user.school
+      end
+   
+      can :create, GroupStudent
+      can :manage, GroupStudent do |gs|
+        gs.group.school == user.school
+      end
+      can :create, Activity
+      can :manage, Activity do |a|
+	a.group.school == user.school
+      end
+    elsif user.teacher?
+      can [:index,:read,:show], Group do |g|
+	g.school == user.school
+      end
     end
     # Define abilities for the passed in user here. For example:
     #
